ae7008b69fd6dd617ab24c78cd6f6bbbe7b84439
Akanle Jesus <105521093+JesusOfLagos@users.noreply.github.com>
2023-01-25 20:00:33 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,19 @@
+const openSubMenu = document.querySelector(".fa-chevron-down");
+const subMenu = document.querySelector(".sub-menu");
+
+openSubMenu.addEventListener("click", function () {
+  if (subMenu.style.display !== "block") {
+    subMenu.style.display = "block";
+  } else {
+    subMenu.style.display = "none";
+  }
+});
+
+// mobile nav
+
+const burger = document.querySelector(".burger");
+const menu = document.querySelector("header nav ul");
+
+burger.addEventListener("click", function () {
+  menu.classList.toggle("open");
+});
